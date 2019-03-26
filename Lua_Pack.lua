876c1b856c8bc2f1c300694964e4a0149c25aca5
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-03-26 12:47:35 -0400

    Update Lua_Pack.lua

--- Lua_Pack.lua
+++ Lua_Pack.lua
@@ -99,7 +99,6 @@ local function menus() if IsButtonPressed(gui_GetValue("msc_menutoggle")) then m
 local scriptName = GetScriptName()
 local scriptFile = "https://raw.githubusercontent.com/Zack2kl/Lua-Pack/master/Lua_Pack.lua"
 local versionFile = "https://raw.githubusercontent.com/Zack2kl/Lua-Pack/master/version.txt"
-local currentVersion = "1.4.2.4.1"
 local currentVersion = "1.4.2.4.2"
 local updateAvailable, newVersionCheck, updateDownloaded = false, true, false
 
