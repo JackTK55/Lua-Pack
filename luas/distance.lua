900299d464565e461ed88e7f26eb0d3517d2b76d
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-08-28 09:50:54 -0400

    Update distance.lua

--- luas/distance.lua
+++ luas/distance.lua
@@ -1,6 +1,6 @@
 -- csgo distance stuff
 local function distance3D(a, b)
-	--distance = math.sqrt( (b[1]-a[1])^2 + (b[2]-a[2])^2 + (b[3]-a[3])^2 )
+	--distance = math.sqrt( (b[1]-a[1])*(b[1]-a[1]) + (b[2]-a[2])*(b[2]-a[2]) + (b[3]-a[3])*(b[3]-a[3]) ) -- *ing it by itself is faster than ^2
 	distance = vector.Distance(a, b)
 
 	return {
