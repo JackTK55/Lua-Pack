8af8eba030e6916149c526f264444526cc04ba02
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-09-23 10:43:40 -0400

    Update Auto Buy.lua
    shortened localizations

--- luas/Auto Buy.lua
+++ luas/Auto Buy.lua
@@ -1,39 +1,36 @@
-local PlayerIndexByUserID, LocalPlayerIndex, GetLocalPlayer, table_concat, client_exec = client.GetPlayerIndexByUserID, client.GetLocalPlayerIndex, entities.GetLocalPlayer, table.concat, client.Command
+local a,b,c,d,e,f,A,B,C=client.GetPlayerIndexByUserID,client.GetLocalPlayerIndex,entities.GetLocalPlayer,table.concat,client.Command,callbacks.Register,gui.Reference,gui.Checkbox,gui.Combobox
 
-local AB_Show = gui.Checkbox(gui.Reference("MISC", "GENERAL", "Main"), "msc_autobuy", "Auto Buy", false)
-local AB_W = gui.Window("AB_W", "Auto Buy", 100, 200, 200, 306)
-local AB_GB = gui.Groupbox(AB_W, "Auto Buy Settings", 16, 17)
-local AB_E = gui.Checkbox(AB_GB, "AB_Active", "Active", false)
-local PrimaryWeapons = gui.Combobox(AB_GB, "AB_Primary_Weapons", "Primary Weapons", '-', "AK | M4", "Scout", "SG553 | AUG", "AWP", "Auto")
-local SecondaryWeapons = gui.Combobox(AB_GB, "AB_Secondary_Weapons", "Secondary Weapons", '-', "Elite", "P250", "Tec-9 | Five-Seven", "R8 | Deagle")
-local AB_M = gui.Multibox(AB_GB, 'Gear')
-local Kev = gui.Checkbox(AB_M, "AB_Armor_K", "Kevlar", false)
-local Kev_Hel = gui.Checkbox(AB_M, "AB_Armor_KH", "Helmet", false)
-local Defuser = gui.Checkbox(AB_M, "AB_Defuser", "Defuse Kit", false)
-local GNade = gui.Checkbox(AB_M, "AB_GNade", "Grenade", false)
-local MNade = gui.Checkbox(AB_M, "AB_MNade", "Molotov", false)
-local SNade = gui.Checkbox(AB_M, "AB_SNade", "Smoke", false)
-local FNade = gui.Checkbox(AB_M, "AB_FNade", "Flashbang", false)
-local Zeus = gui.Checkbox(AB_M, "AB_Zeus", "Zeus", false)
-local AB_buyAbove = gui.Slider(AB_GB, 'AB_buyAboveAmount', 'Buy if $ is Above (value*1000)', 3.7, 0, 16)
+local h = B(A("MISC","GENERAL","Main"),"msc_autobuy","AutoBuy",false)
+local i = gui.Window("AB_W","AutoBuy",100,200,200,306)
+local j = gui.Groupbox(i,"AutoBuy Settings",16,17)
+local k = B(j,"AB_Active","Active",false)
+local l = C(j,"AB_Primary_Weapons","Primary Weapons",'-',"AK | M4","Scout","SG553 | AUG","AWP","Auto")
+local m = C(j,"AB_Secondary_Weapons","Secondary Weapons",'-',"Elite","P250","Tec-9 | Five-Seven","R8 | Deagle")
+local n = gui.Multibox(j,'Gear')
+local o = {B(n,"AB_Armor_K","Kevlar",false),B(n,"AB_Armor_KH","Helmet",false),B(n,"AB_Defuser","Defuse Kit",false),B(n,"AB_GNade","Grenade",false),B(n,"AB_MNade","Molotov",false),B(n,"AB_SNade","Smoke",false),B(n,"AB_FNade","Flashbang",false),B(n,"AB_Zeus","Zeus",false)}
+local p = gui.Slider(j,'AB_buyAboveAmount','Buy if $ is Above(value*1000)',3.7,0,16)
+local q,r,s = {'','buy"ak47"','buy"ssg08"','buy"sg556"','buy"awp"','buy"scar20"'},{'','buy"elite"','buy"p250"','buy"tec9"','buy"deagle"'},{'buy"vest"','buy"vesthelm"','buy"defuser"','buy"hegrenade"','buy"molotov";buy"incgrenade"','buy"smokegrenade"','buy"flashbang"','buy"taser"'}
 
-callbacks.Register('Draw', 'autobuy show menu', function()
-	AB_W:SetActive(AB_Show:GetValue() and gui.Reference('MENU'):IsActive())
-end)
-
-local primary_weapon = {'', 'buy "ak47"; ', 'buy "ssg08"; ', 'buy "sg556"; ', 'buy "awp"; ', 'buy "scar20"; '}
-local secondary_weapon = {'', 'buy "elite"; ', 'buy "p250"; ', 'buy "tec9"; ', 'buy "deagle"; '}
+f('Draw',function() i:SetActive(h:GetValue() and A('MENU'):IsActive()) end)
 
 client.AllowListener('player_spawn')
-callbacks.Register("FireGameEvent", 'Auto Buy', function(e)
-	if not AB_E:GetValue() or GetLocalPlayer() == nil or e:GetName() ~= 'player_spawn' or PlayerIndexByUserID(e:GetInt("userid")) ~= LocalPlayerIndex() then
+f("FireGameEvent",'autobuy',function(y)
+	local t, u = y:GetName(), y:GetInt("userid")
+	if not k:GetValue() or c() == nil or t ~= 'player_spawn' or a(u) ~= b() then
 		return
 	end
 
-	local money = GetLocalPlayer():GetProp('m_iAccount')
-	local buy_items = table_concat({primary_weapon[PrimaryWeapons:GetValue() + 1] or '', secondary_weapon[SecondaryWeapons:GetValue() + 1] or '', Kev:GetValue() and 'buy "vest"; ' or '', Kev_Hel:GetValue() and 'buy "vesthelm"; ' or '', Defuser:GetValue() and 'buy "defuser"; ' or '', GNade:GetValue() and 'buy "hegrenade"; ' or '', MNade:GetValue() and 'buy "molotov"; buy "incgrenade"; ' or '', SNade:GetValue() and 'buy "smokegrenade"; ' or '', FNade:GetValue() and 'buy "flashbang"; ' or '', Zeus:GetValue() and 'buy "taser"; ' or ''}, '')
+	local v, x = {}, c():GetProp('m_iAccount')
+	v[1], v[2] = q[l:GetValue()+1], r[m:GetValue()+1]
 
-	if money >= AB_buyAbove:GetValue()*1000 or money < 1 then
-		client_exec(buy_items, true)
+	for z=1,#o do
+		if o[z]:GetValue()then
+			v[#v+1] = s[z]
+		end
+	end
+
+	local v = d(v,';')
+	if x >= p:GetValue()*1000 or x < 1 then
+		e(v,true)
 	end
 end)
