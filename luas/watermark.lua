652d4baaacdf236f284b059959e514d8982ccdff
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-10-11 09:24:07 -0400

    Update watermark.lua

--- luas/watermark.lua
+++ luas/watermark.lua
@@ -1,123 +1,102 @@
-local GetMousePos, IsButtonDown, GetLocalPlayerIndex, GetPlayerResources, draw_Color, draw_FilledRect, draw_TextShadow, common_Time, GetLocalPlayer, string_format, os_date, draw_GetTextSize=input.GetMousePos,input.IsButtonDown,client.GetLocalPlayerIndex,entities.GetPlayerResources,draw.Color,draw.FilledRect,draw.TextShadow,common.Time,entities.GetLocalPlayer,string.format,os.date,draw.GetTextSize
-local move_watermark = gui.Checkbox(gui.Reference('SETTINGS', 'Miscellaneous'), 'lua_time', 'Moving Watermark', false)
+local a,b,c,d,e,f,g,h,i,j,k,l,m,n=input.IsButtonDown,client.GetLocalPlayerIndex,entities.GetPlayerResources,draw.Color,draw.FilledRect,draw.TextShadow,common.Time,entities.GetLocalPlayer,string.format,os.date,draw.GetTextSize,client.GetConVar('name'),math.sin,draw.GetScreenSize
 
-local x, y = 869, 60
-local w, h = x + 182, y + 21
-local height = h
+local y = 60
+local z, o = y + 21, {}
 
-local window_x_offset, window_y_offset
-
-local function inside_box(a, b)
-	return a[1] > b[1] and a[1] < b[3] and a[2] > b[2] and a[2] < b[4]
+local function t(a)
+	local b = a/2
+	return m(g() * 0.6) * b + b
 end
 
-local function window_moving()
-	local mouse_x, mouse_y = GetMousePos()
-	local mouse_down = IsButtonDown(1)
+local function p()
+	local d,e,f,g,u = c():GetPropInt("m_iKills", b()),c():GetPropInt("m_iAssists", b()),c():GetPropInt("m_iDeaths", b()),c():GetPropInt("m_iPing", b()),0
 
-	if mouse_down and moving_window then
-		w = x + 182
-		h = y + 20
-
-		x = mouse_x - window_x_offset
-		y = mouse_y - window_y_offset
-		height = y + 21
-	end
-
-	if mouse_down and inside_box({mouse_x, mouse_y}, {x, y, w, h}) then
-		window_x_offset = mouse_x - x
-		window_y_offset = mouse_y - y
-		moving_window = true
-	end
-
-    if moving_window and not mouse_down then
-        moving_window = false
-        window_x_offset = 0
-        window_y_offset = 0
-    end
-end
-
-local stats = {}
-local function get_stats()
-	local kills = GetPlayerResources():GetPropInt("m_iKills", GetLocalPlayerIndex())
-	local assists = GetPlayerResources():GetPropInt("m_iAssists", GetLocalPlayerIndex())
-	local deaths = GetPlayerResources():GetPropInt("m_iDeaths", GetLocalPlayerIndex())
-	local ping = GetPlayerResources():GetPropInt("m_iPing", GetLocalPlayerIndex())
-
-	if deaths == 0 then
-		kd = kills
+	if f == 0 then
+		u = d
 	else
-		kd = string_format('%.2f', kills/deaths)
+		u = i('%.2f', d/f)
 	end
 
-	stats = {
-		'Kills: '.. kills,
-		'Assists: '.. assists,
-		'Deaths: '.. deaths,
-		'K/D: '.. kd, 
-		'Ping: '.. ping	
+	o = {
+		'Kills: '.. d,
+		'Assists: '.. e,
+		'Deaths: '.. f,
+		'K/D: '.. u,
+		'Ping: '.. g	
 	}
 end
 
-local function draw_scoreboard()
-	get_stats()
+local function q()
+	p()
 
-	draw_Color(100, 100, 100, 180)
-	draw_FilledRect(x, height, w, height + 69)
+	local a, b = n()
+	local c = a / 2
 
-	draw_Color(255, 35, 43, 255)
-	for i=1, #stats do
-		draw_TextShadow(x + 12, y + 12 + (i*12), stats[i])
+	d(100, 100, 100, 180)
+	e(c - 75, z, c + 74, z + 65)
+
+	d(255, 35, 43, 255)
+	for i=1, #o do
+		local a, b = k(o[i])
+		f(c - (a/2), (z-10) + (i*12), o[i])
 	end
 end
 
-local function draw_welcome()
-	draw_Color(100, 100, 100, 180)
-	draw_FilledRect(x, height, w, height + 25)
+local function r()
+	local a, b = n()
+	local c = a / 2
 
-	local tw, th = draw_GetTextSize('Welcome back')
-	local tw2, th2 = draw_GetTextSize('Zack')
+	local v,w = k('Welcome back')
+	local g, h = v / 2, z + (w / 2)-2
+	
+	local x,_ = k(l)
+	local i = x / 2
+	
+	local x = c - g - i
+	local w = c + g
 
-	draw_Color(255, 35, 43, 255)
-	draw_TextShadow(x + (tw/2) , y + 27 , 'Welcome back')
+	d(100, 100, 100, t(180))
+	e(x - 7, z, w + i + 7, z + 21)
 
-	draw_Color(32, 255, 35, 255)
-	draw_TextShadow(x + (tw + tw2*2) - 4 , y + 27 , 'Zack')
+	d(255, 100, 43, t(255))
+	f(x - 2, h, 'Welcome back')
+
+	d(32, 255, 35, t(255))
+	f(w - i + 2, h, l)
 end
 
-local function draw_watermark()
-	if common_Time() < 7 then
-		draw_welcome()
+local function s()
+	if g() <= 8 then
+		r()
 	end
 
-	if move_watermark:GetValue() then
-		window_moving()
+	if h() ~= nil then
+		y = 60
+		z = y + 21
+
+		if a('tab') then
+			q()
+		end
 	else
-		if GetLocalPlayer() ~= nil then		
-			y = 60
-			height = y + 21
-		else
-			y, height = 0, 21
-		end
+		y = 0
+		z = y + 21
 	end
+	
+	local a,_ = n()
+	local b = a / 2
 
-	if GetLocalPlayer() ~= nil then
-		if IsButtonDown('tab') then
-			draw_scoreboard()
-		end
-	end
+	local r = j('%A, %B %d, %H:%M:%S')
+	local v, w = k(r)
+	local c, g = v / 2, w / 2
 
-	draw_Color(50, 50, 50, 180)
-	draw_FilledRect(x, y, w, height)
+	d(50, 50, 50, 180)
+	e(b - c - 5, y, b + c + 5, z)
 
-	local time = os_date('%A, %B %d, %H:%M:%S')
-	local text_width, text_height = draw_GetTextSize(time)
-
-	draw_Color(255, 35, 43, 255)
-	draw_TextShadow(x + 12 + (text_width/w), y + (text_height/2) - 2, time)
+	d(255, 35, 43, 255)
+	f(b - c, y + g - 2, r)
 end
 
-local function drawing()
-	draw_watermark()
+local function p()
+	s()
 end
-callbacks.Register('Draw', drawing)
+callbacks.Register('Draw', p)
