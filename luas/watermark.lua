cf6ed06346d9a023280657f49eaa1ed927cd7886
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-10-12 08:00:14 -0400

    Update watermark.lua

--- luas/watermark.lua
+++ luas/watermark.lua
@@ -1,7 +1,15 @@
 local a,b,c,d,e,f,g,h,i,j,k,l,m,n=input.IsButtonDown,client.GetLocalPlayerIndex,entities.GetPlayerResources,draw.Color,draw.FilledRect,draw.TextShadow,common.Time,entities.GetLocalPlayer,string.format,os.date,draw.GetTextSize,client.GetConVar('name'),math.sin,draw.GetScreenSize
 
-local y = 60
-local z, o = y + 21, {}
+local A = gui.Multibox(gui.Reference('SETTINGS', 'Miscellaneous'), 'Watermark Options')
+local B = gui.Checkbox(A, 'lua_wm_kills', 'Kills', false)
+local C = gui.Checkbox(A, 'lua_wm_assists', 'Assists', false)
+local D = gui.Checkbox(A, 'lua_wm_deaths', 'Deaths', false)
+local E = gui.Checkbox(A, 'lua_wm_kd', 'KD', false)
+local F = gui.Checkbox(A, 'lua_wm_ping', 'Ping', false)
+
+local x = false -- show stuff
+local y,z,o = 0,0,{}
+local v = '%A, %B %d, %H:%M:%S' -- date format
 
 local function t(a)
 	local b = a/2
@@ -9,7 +17,7 @@ local function t(a)
 end
 
 local function p()
-	local d,e,f,g,u = c():GetPropInt("m_iKills", b()),c():GetPropInt("m_iAssists", b()),c():GetPropInt("m_iDeaths", b()),c():GetPropInt("m_iPing", b()),0
+	local d,e,f,g,u = c():GetPropInt('m_iKills', b()),c():GetPropInt('m_iAssists', b()),c():GetPropInt('m_iDeaths', b()),c():GetPropInt('m_iPing', b()),0
 
 	if f == 0 then
 		u = d
@@ -17,33 +25,56 @@ local function p()
 		u = i('%.2f', d/f)
 	end
 
-	o = {
-		'Kills: '.. d,
-		'Assists: '.. e,
-		'Deaths: '.. f,
-		'K/D: '.. u,
-		'Ping: '.. g	
-	}
+	o = {}
+
+	if B:GetValue() then
+		o[#o + 1] = 'Kills: '.. d
+	end
+	
+	if C:GetValue() then
+		o[#o + 1] = 'Assists: '.. e
+	end
+	
+	if D:GetValue() then
+		o[#o + 1] = 'Deaths: '.. f
+	end
+	
+	if E:GetValue() then
+		o[#o + 1] = 'K/D: '.. u
+	end
+	
+	if F:GetValue() then
+		o[#o + 1] = 'Ping: '.. g
+	end
+	
 end
 
 local function q()
 	p()
 
-	local a, b = n()
+	if #o == 0 then
+		return
+	end
+
+	local a,_ = n()
 	local c = a / 2
+	
+	local r = j(v)
+	local v,_ = k(r)
+	local a = v / 2
 
 	d(100, 100, 100, 180)
-	e(c - 75, z, c + 74, z + 65)
+	e(c - a - 5, z, c + a + 5, z + (#o*12) + 5)
 
 	d(255, 35, 43, 255)
 	for i=1, #o do
-		local a, b = k(o[i])
+		local a,_ = k(o[i])
 		f(c - (a/2), (z-10) + (i*12), o[i])
 	end
 end
 
 local function r()
-	local a, b = n()
+	local a,_ = n()
 	local c = a / 2
 
 	local v,w = k('Welcome back')
@@ -74,7 +105,7 @@ local function s()
 		y = 60
 		z = y + 21
 
-		if a('tab') then
+		if a('tab') or x then
 			q()
 		end
 	else
@@ -85,7 +116,7 @@ local function s()
 	local a,_ = n()
 	local b = a / 2
 
-	local r = j('%A, %B %d, %H:%M:%S')
+	local r = j(v)
 	local v, w = k(r)
 	local c, g = v / 2, w / 2
 
@@ -99,4 +130,5 @@ end
 local function p()
 	s()
 end
+
 callbacks.Register('Draw', p)
