2fd74e894e6215662d91b0271e37963edcca6d65
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-10-14 14:33:40 -0400

    Update Knife on Left Hand.lua

--- luas/Knife on Left Hand.lua
+++ luas/Knife on Left Hand.lua
@@ -1,16 +1,18 @@
-local GetLocalPlayer, client_exec, string_format = entities.GetLocalPlayer, client.Command, string.format
+local a,b,c = entities.GetLocalPlayer,client.Command,string.format
 
-local K_O_L_H = gui.Checkbox(gui.Reference("MISC", "GENERAL", "Main"), "msc_knifelefthand", "Knife On Left Hand", false)
+local d = gui.Checkbox(gui.Reference("MISC","GENERAL","Main"),"msc_knifelefthand","Knife On Left Hand",false)
 
-callbacks.Register("Draw", 'Knife on Left Hand', function()
-	if not K_O_L_H:GetValue() or GetLocalPlayer() == nil then
+local function i()
+	if not d:GetValue() or a() == nil then
 		return
 	end
-	
-	local weapon = GetLocalPlayer():GetPropEntity("m_hActiveWeapon")
-	local alive = weapon ~= nil and GetLocalPlayer():IsAlive()
-	local _val = alive and weapon:GetClass() == "CKnife"
-	local command = string_format('cl_righthand %i', _val and 0 or 1)
 
-	client_exec(command, true)
-end)
+	local e = a():GetPropEntity("m_hActiveWeapon")
+	local f = e ~= nil and a():IsAlive()
+	local g = f and e:GetClass() == "CKnife"
+	local h = c('cl_righthand %i',g and 0 or 1)
+
+	b(h, true)
+end
+
+callbacks.Register("Draw",i)
