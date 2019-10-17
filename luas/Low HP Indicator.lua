9800fadfdbdf39c2252218751cc06ac7a02c0fc5
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-10-16 20:27:14 -0400

    Update Low HP Indicator.lua

--- luas/Low HP Indicator.lua
+++ luas/Low HP Indicator.lua
@@ -1,28 +1,37 @@
-local lowHPIndicator = gui.Checkbox(gui.Reference('SETTINGS', 'Miscellaneous'), 'lua_lowhp_ind', "Low HP Indicator", false)
-local font = draw.CreateFont("Tahoma", 13, 200)
+local GetLocalPlayer,draw_GetScreenSize,string_format,draw_SetFont,draw_GetTextSize,draw_Color,draw_Text,entities_FindByClass,pos=entities.GetLocalPlayer,draw.GetScreenSize,string.format,draw.SetFont,draw.GetTextSize,draw.Color,draw.Text,entities.FindByClass,0
+local lowHPIndicator = gui.Checkbox(gui.Reference('SETTINGS','Miscellaneous'), 'lua_lowhp_ind', "Low HP Indicator", false)
+local font = draw.CreateFont('Tahoma', 12)
+local hp_max = 5
 
-local pos = 0
-function drawLowHP()
+local function drawLowHP()
 	if not lowHPIndicator:GetValue() then
 		return
 	end
 
-	local screenx, screeny = draw.GetScreenSize()
-	local pos = 0
-	local players = entities.FindByClass('CCSPlayer')
+	local _,y = draw_GetScreenSize()
+	local y_half, pos = y/2, 0
+	local players = entities_FindByClass('CCSPlayer')
+
 	for i=1, #players do
 		local player = players[i]
-		if player:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber() then
-			local playername = player:GetName()
-			local HP = player:GetProp("m_iHealth")
-			local location = player:GetProp('m_szLastPlaceName')
+		local is_enemy = player:GetTeamNumber() ~= GetLocalPlayer():GetTeamNumber()
 
-			local str = string.format("%s - %s (%i HP)", location, playername, HP)
-			draw.SetFont(font)
-			local tW, tH = draw.GetTextSize(playername)
-			if HP < 5 and HP > 0 then
-				draw.Color(255, 255, 255, 255)
-				draw.Text(3, (screeny/2) + pos, str)
+		if is_enemy and player:IsAlive() then
+			local playername, hp, location = player:GetName(), player:GetPropInt('m_iHealth'), player:GetPropString('m_szLastPlaceName')
+
+			if location == 'unknown' then 
+				location = 'nearby' 
+			end
+
+			local str = string_format('%s - %s (%i HP)', location, playername, hp)
+			local low = hp <= hp_max
+
+			draw_SetFont(font)
+			local _,tH = draw_GetTextSize(str)
+
+			if low then
+				draw_Color(255, 255, 255, 255)
+				draw_Text(3, y_half + pos, str)
 				pos = pos + tH
 			end
 		end
