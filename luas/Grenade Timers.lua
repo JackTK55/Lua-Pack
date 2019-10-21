77f31d7431cc6787ddcc63ee0778538be3ed94d6
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-10-21 07:55:25 -0400

    Update Grenade Timers.lua

--- luas/Grenade Timers.lua
+++ luas/Grenade Timers.lua
@@ -9,7 +9,7 @@ for i=1, #listeners do
 end
 
 local smokes, molotovs, flashes, grenades = {}, {}, {}, {}
-local function grenade_timer_events(e)
+local function events(e)
 	local event_name = e:GetName()
 
 	if not better_grenades:GetValue() or event_name ~= "round_prestart" and
@@ -41,8 +41,8 @@ local function grenade_timer_events(e)
 		molotovs[#molotovs + 1] = {g_curtime(), entityid, x, y, z, user_index}
 	end
 
-	if event_name == "inferno_expire" or event_name == "inferno_extinguish" then 
-		for k,v in pairs(molotovs) do 
+	if event_name == "inferno_expire" or event_name == "inferno_extinguish" then
+		for k,v in pairs(molotovs) do
 			if v[2] == entityid then
 				molotovs[k] = nil
 			end
@@ -61,57 +61,57 @@ local function grenade_timer_events(e)
 	end
 end
 
-local function smoke_and_molotov_timers()
+local function sm_timers()
 	if not better_grenades:GetValue() or GetLocalPlayer() == nil then
 		return
 	end
 
-	for k,v in pairs(smokes) do 
-		if v[1] - g_curtime() + 17.6 > 0 then 
-			local X, Y = client_WorldToScreen(v[3], v[4], v[5]) 
-			local smoke_timeleft = string_format("%0.1f",  v[1] - g_curtime() + 17.6) 
-			if X ~= nil and Y ~= nil then 
+	for k,v in pairs(smokes) do
+		if v[1] - g_curtime() + 17.6 > 0 then
+			local X, Y = client_WorldToScreen(v[3], v[4], v[5])
+			local smoke_timeleft = string_format("%0.1f",  v[1] - g_curtime() + 17.6)
+			if X ~= nil and Y ~= nil then
 				draw_SetFont(Vf11)
-				local tW, tH = draw_GetTextSize(smoke_timeleft) 
-				local tW2, tH2 = draw_GetTextSize("SMOKE") 
-				draw_Color(255, 255, 255, 255) 
-				draw_TextShadow(X - (tW/2), Y - (tH/2), smoke_timeleft) 
-				draw_Color(255, 255, 255, 255) 
-				draw_TextShadow(X - (tW2/2), Y - (tH2/2) + tH2, "SMOKE") 
+				local tW, tH = draw_GetTextSize(smoke_timeleft)
+				local tW2, tH2 = draw_GetTextSize("SMOKE")
+				draw_Color(255, 255, 255, 255)
+				draw_TextShadow(X - (tW/2), Y - (tH/2), smoke_timeleft)
+				draw_Color(255, 255, 255, 255)
+				draw_TextShadow(X - (tW2/2), Y - (tH2/2) + tH2, "SMOKE")
 			end
 		else
 			smokes[k] = nil
-		end 
-	end 
+		end
+	end
+	
+	for k,v in pairs(molotovs) do
+		if v[1] - g_curtime() + 7 > 0 then
+			local X, Y = client_WorldToScreen(v[3], v[4], v[5])
+			local molotov_timeleft = string_format("%0.1f",  v[1] - g_curtime() + 7)
 
-	for k,v in pairs(molotovs) do 
-		if v[1] - g_curtime() + 7 > 0 then 
-			local X, Y = client_WorldToScreen(v[3], v[4], v[5]) 
-			local molotov_timeleft = string_format("%0.1f",  v[1] - g_curtime() + 7) 
-			
 			if X ~= nil and Y ~= nil then
-				draw_SetFont(Vf11) 
-				local tW, tH = draw_GetTextSize(molotov_timeleft) 
-				local tW2, tH2 = draw_GetTextSize("MOLLY") 
-				draw_Color(255, 255, 255, 255) 
+				draw_SetFont(Vf11)
+				local tW, tH = draw_GetTextSize(molotov_timeleft)
+				local tW2, tH2 = draw_GetTextSize("MOLLY")
+				draw_Color(255, 255, 255, 255)
 				draw_TextShadow(X - (tW/2), Y - (tH/2), molotov_timeleft)
-				
+
 				if entities_GetByIndex(v[6]):GetTeamNumber() ~= GetLocalPlayer():GetTeamNumber() then
-					r, g, b, a = 251, 82, 79, 255 
-				else 
-					r, g, b, a = 153, 153, 255, 255 
-				end 
-					
-				draw_Color(r, g, b, a) 
-				draw_TextShadow(X - (tW2/2), Y - (tH2/2) + tH2, "MOLLY") end 
-			
-		else 
+					r, g, b, a = 251, 82, 79, 255
+				else
+					r, g, b, a = 153, 153, 255, 255
+				end
+
+				draw_Color(r, g, b, a)
+				draw_TextShadow(X - (tW2/2), Y - (tH2/2) + tH2, "MOLLY")
+			end
+		else
 			molotovs[k] = nil
-		end 
-	end 
+		end
+	end
 end
 
-local function grenade_and_flash_timers(b)
+local function gf_timers(b)
 	if not better_grenades:GetValue() or b:GetEntity():GetClass() ~= "CBaseCSGrenadeProjectile" then
 		return
 	end
@@ -137,6 +137,6 @@ local function grenade_and_flash_timers(b)
 	end
 end
 
-Register("Draw", smoke_and_molotov_timers) 
-Register("DrawESP", grenade_and_flash_timers) 
-Register("FireGameEvent", grenade_timer_events)
+Register("Draw", sm_timers)
+Register("DrawESP", gf_timers)
+Register("FireGameEvent", events)
