5cd3c99c8e84ed55605c07c387b990ece7a26e05
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-11-16 06:42:41 -0500

    Update admin_spectating_notify.lua

--- luas/admin_spectating_notify.lua
+++ luas/admin_spectating_notify.lua
@@ -1,10 +1,10 @@
-local MaxClients, GetPlayerInfo, Reg, GetMousePos, IsButtonPressed, GetValue, Command, get_local_player, GetByUserID, Text, Color, GetTextSize, OutRect, RectFill = globals.MaxClients, client.GetPlayerInfo, callbacks.Register, input.GetMousePos, input.IsButtonPressed, gui.GetValue, client.Command, entities.GetLocalPlayer, entities.GetByUserID, draw.Text, draw.Color, draw.GetTextSize, draw.OutlinedRect, draw.FilledRect
-
-local Window = gui.Window('lua_custom_playerlist_window', 'Player List', 200, 200, 200, 277)
-local custom_sound = gui.Editbox(Window, 'lua_player_list_admin_notify_sound', 'custom_sound.mp3')
+local MaxClients, GetPlayerInfo, Reg, GetMousePos, IsButtonPressed, GetValue, get_local_player, GetByUserID, Text, Color, GetTextSize, RoundedRect, RoundedRectFill, Command = globals.MaxClients, client.GetPlayerInfo, callbacks.Register, input.GetMousePos, input.IsButtonPressed, gui.GetValue, entities.GetLocalPlayer, entities.GetByUserID, draw.Text, draw.Color, draw.GetTextSize, draw.RoundedRect, draw.RoundedRectFill, client.Command
 
 local listen = {'game_init', 'player_disconnect', 'player_connect', 'player_changename'}
 for i=1, #listen do client.AllowListener(listen[i]) end
+
+local Window = gui.Window('lua_custom_playerlist_window', 'Player List', 200, 200, 200, 263)
+local custom_sound = gui.Editbox(Window, 'lua_player_list_admin_notify_sound', 'custom_sound.mp3')
 
 local inside_area=function(x,y,x1,y1,x2,y2) local X,Y = x > x1 and x < x2, y > y1 and y < y2 return X and Y end
 local in_my_game=function(i) for a=1, MaxClients() do local pInfo = GetPlayerInfo(a) if pInfo ~= nil then if i == pInfo['SteamID'] then return true end end end return false end
@@ -25,7 +25,7 @@ local function get_players()
 			local IsBot = player_info['IsBot']
 			local IsGOTV = player_info['IsGOTV']
 
-			if not IsGOTV then -- if not IsGOTV and not IsBot then
+			if not IsGOTV then
 				if list_of_players[SteamID] == nil then
 					list_of_players[SteamID] = player_info
 				end
@@ -37,31 +37,31 @@ end
 local function update_list(x, y, w, h, a)
 	local mX, mY = GetMousePos()
 	local y = y - 16
-	local gap = 12
+	local gap = 6
 
 	for s,v in pairs(list_of_players) do
 		local name = v['Name']
 
 		local tW, tH = GetTextSize(name)
-		local tWh = tW / 2
-		gap = gap + tH
+		local tWh = tW * 0.5
+		gap = gap + tH + 2
 		local g = y + gap
-		local yMath = g - (tH/2)
-		local hMath = g + (tH/2)
-		local width = ( (w-x)/2 ) - tWh
+		local yMath = g - (tH*0.5)
+		local hMath = g + (tH*0.5)
+		local width = ( (w-x)*0.5 ) - tWh
 
 		if inside_area(mX,mY, x,yMath,w,hMath) then
 			if IsButtonPressed(1) then
 				v['IsSelected'] = not v['IsSelected']
 			else
 				Color(GetValue('clr_gui_hover'))
-				RectFill(x, yMath, w, hMath)
+				RoundedRectFill(x, yMath, w, hMath)
 			end
 		end
 
 		if v['IsSelected'] then
 			Color(GetValue('clr_gui_listbox_active'))
-			RectFill(x+2, yMath, w-2, hMath)
+			RoundedRectFill(x+2, yMath, w-2, hMath)
 		end
 
 		Color(GetValue('clr_gui_text2'))
@@ -73,23 +73,23 @@ local function update_list(x, y, w, h, a)
 	end
 
 	Color(GetValue('clr_gui_listbox_outline'))
-	OutRect(x,y+12,w, (y+gap+13)+(#list_of_players*gap))
+	RoundedRect(x,y+12,w, (y+gap+9)+(#list_of_players*gap))
 end
 
-local Custom = gui.Custom(gui.Groupbox(Window,'Players',16,50,168,180),'lua_custom_playerlist_list',0,0,135,384,update_list)
+local Custom = gui.Custom(gui.Groupbox(Window,'Players',16,47,168,172),'lua_custom_playerlist_list',0,4,136,286,update_list)
 
 Reg('Draw', function()
 	Window:SetActive(gui.Reference('MENU'):IsActive())
 
-	local local_player = get_local_player()
-	if local_player == nil then
+	local lp = get_local_player()
+	if lp == nil then
 		if #list_of_players ~= 0 then list_of_players = {} end
 		return
 	end
 
-	local localplayerindex = local_player:GetIndex()
+	local lpi = lp:GetIndex()
 
-	if not local_player:IsAlive() then
+	if not lp:IsAlive() then
 		return
 	end
 
@@ -100,7 +100,7 @@ Reg('Draw', function()
 				local ObserverTarget = ent:GetPropEntity("m_hObserverTarget")
 				if ObserverTarget ~= nil then
 					local TargetIndex = ObserverTarget:GetIndex()
-					if TargetIndex == localplayerindex then
+					if TargetIndex == lpi then
 						if not v['IsSpectatingMe'] then
 							Command('playvol '..custom_sound:GetValue()..' 1', true)
 							v['IsSpectatingMe'] = true
