c2d0e462a2e30e5aa35524bb6774bbe09d5bcf06
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-11-22 07:11:34 -0500

    Update Fast Plant.lua

--- luas/Fast Plant.lua
+++ luas/Fast Plant.lua
@@ -1,6 +1,6 @@
 -- If you want it to do it automatically remove lines with -- on them
 
-local GetLocalPlayer, IsButtonDown = entities.GetLocalPlayer, input.IsButtonDown
+local GetLocalPlayer, IsButtonDown, Command = entities.GetLocalPlayer, input.IsButtonDown, client.Command
 local r = gui.Reference('MISC', 'AUTOMATION', 'Other')
 local enabled = gui.Checkbox(r, 'fast_plant', 'Fast Plant', false)
 local hotkey = gui.Keybox(r, 'fast_plant_key', 'Fast Plant Key', 0) -- Don't make it "e" or "mouse 1"
@@ -17,10 +17,15 @@ callbacks.Register('CreateMove', function(cmd)
 
 	local lp = GetLocalPlayer()
 	local weapon = lp:GetPropEntity('m_hActiveWeapon')
-	local weapon = weapon ~= nil and weapon:GetClass() == 'CC4'
-	local in_zone = weapon and lp:GetPropInt('m_bInBombZone') == 1 
+	local weapon = weapon ~= nil and weapon:GetClass()
 
-	if in_zone then 
-		cmd:SetButtons( (1 << 5) )
-	end 
+	if weapon ~= 'CC4' then
+		Command('slot5', true)
+	else
+		local in_zone = weapon and lp:GetPropInt('m_bInBombZone') == 1 
+
+		if in_zone then 
+			cmd:SetButtons( (1 << 5) )
+		end 
+	end
 end)
