65e9de6f9f5cd40518e7099cc04ea474180da387
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-11-23 09:20:33 -0500

    Update Scope Fix.lua

--- luas/Scope Fix.lua
+++ luas/Scope Fix.lua
@@ -1,29 +1,29 @@
 local a,b,c = entities.GetLocalPlayer,gui.GetValue,gui.SetValue
 local d = gui.Checkbox(gui.Reference("VISUALS","Shared"),"vis_fixfov","Fix Scoped FOV",false)
-local e, f, g, h
-local i = false
+local i,e
 
-local function j()
-	if not d:GetValue() or a() == nil then
+callbacks.Register("Draw",function()
+	if not d:GetValue() then
 		return
 	end
 
-	local g = a():GetProp("m_bIsScoped")
+	local lp = a()
+	if lp == nil then
+		return
+	end
+
+	local g = lp:GetProp("m_bIsScoped")
 	local h = g == 1 or g == 257
 
 	if h then
 		c("vis_view_fov", 0)
-		c("vis_view_model_fov", 0)
 		i = true
 	else
 		if not i then
-			e, f = b("vis_view_fov"), b("vis_view_model_fov")
+			e = b("vis_view_fov")
 		else
 			c("vis_view_fov", e)
-			c("vis_view_model_fov", f)
 			i = false
 		end
 	end
-end
-
-callbacks.Register("Draw",j)
+end)
