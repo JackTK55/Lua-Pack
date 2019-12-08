32aceeb4ee80f04b46514eb67ffbadb9064429be
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-12-07 21:53:51 -0500

    Create Auto Update.lua
    Should work

---
+++ luas/Auto Update.lua
@@ -0,0 +1,20 @@
+local cS = GetScriptName()
+local cV = '1.0.0'
+local gS = ' PUT LINK TO RAW LUA SCRIPT '
+local gV = ' PUT LINK TO RAW VERSION '
+
+callbacks.Register('Draw', 'Auto Update', function()
+	if gui.GetValue('lua_allow_http') and gui.GetValue('lua_allow_cfg') then
+		local nV = http.Get(gV)
+		if cV ~= nV then
+			local nF = http.Get(gS)
+			local cF = file.Open(cS, 'w')
+			cF:Write(nF)
+			cF:Close()
+			print(cS, 'updated from', cV, 'to', nV)
+		else
+			print(cS, 'is up-to-date.')
+			callbacks.Unregister('Auto Update')
+		end
+	end
+end)
