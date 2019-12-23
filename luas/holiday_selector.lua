c646c73c482bc323e37efdfbad72a318eabc5f32
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-12-23 13:17:52 -0500

    Update holiday_selector.lua

--- luas/holiday_selector.lua
+++ luas/holiday_selector.lua
@@ -1,12 +1,12 @@
 local SetConVar = client.SetConVar
-local modes = gui.Combobox(gui.Reference('VISUALS', 'MISC', 'World'), 'vis_holiday', 'Holiday Mode', 'None', 'Halloween', 'Christmas')
-local new_mode
+local new_modes = gui.Combobox(gui.Reference('VISUALS', 'MISC', 'World'), 'vis_holiday', 'Holiday Mode', 'None', 'Halloween', 'Christmas')
+local mode
 
 callbacks.Register('CreateMove', function(_)
-	local mode = modes:GetValue()
+	local new_mode = new_modes:GetValue()
 
-	if new_mode ~= mode then
-		SetConVar('sv_holiday_mode', mode)
-		new_mode = mode
+	if mode ~= new_mode then
+		SetConVar('sv_holiday_mode', new_mode)
+		mode = new_mode
 	end
 end)
