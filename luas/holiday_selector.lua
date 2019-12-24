ad9a9ebbd3e57a0029538ca18959ab55e57c43f5
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2019-12-24 11:50:24 -0500

    Update holiday_selector.lua
    changed callback Draw \
    GetMapName() ~= nil and new_mode or 0

--- luas/holiday_selector.lua
+++ luas/holiday_selector.lua
@@ -1,9 +1,10 @@
-local SetConVar = client.SetConVar
+local SetConVar, GetMapName = client.SetConVar, engine.GetMapName
 local new_modes = gui.Combobox(gui.Reference('VISUALS', 'MISC', 'World'), 'vis_holiday', 'Holiday Mode', 'None', 'Halloween', 'Christmas')
 local mode
 
-callbacks.Register('CreateMove', function(_)
-	local new_mode = new_modes:GetValue()
+--callbacks.Register('CreateMove', function(_)
+callbacks.Register('Draw', function()
+	local new_mode = GetMapName() ~= nil and new_modes:GetValue() or 0
 
 	if mode ~= new_mode then
 		SetConVar('sv_holiday_mode', new_mode)
