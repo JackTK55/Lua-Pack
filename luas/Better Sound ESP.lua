7fbbc0fe46eaebac0a61d723861ae81641cae1b1
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-01-03 16:11:00 -0500

    Create Better Sound ESP.lua

---
+++ luas/Better Sound ESP.lua
@@ -0,0 +1,33 @@
+local g_curtime,PlayerIndexByUserID,pairs,remove = globals.CurTime,client.GetPlayerIndexByUserID,pairs,table.remove
+
+local noises = {'weapon_fire', 'player_footstep', 'weapon_reload'}
+for i=1,#noises do client.AllowListener(noises[i]) end
+local made_noise, dur = {}, 1.25
+
+callbacks.Register('FireGameEvent', function(e)
+	for i=1,#noises do
+		local noise = noises[i]
+		if noise == e:GetName() then
+			local player_index = PlayerIndexByUserID(e:GetInt('UserID'))
+			made_noise[player_index] = {noise, g_curtime() + dur}
+		end
+	end
+end)
+
+callbacks.Register('DrawESP', function(b)
+	local abs = entities.GetLocalPlayer():GetAbsOrigin()
+	local ent = b:GetEntity()
+	local x,y,w,h = b:GetRect()
+
+	for i,v in pairs(made_noise) do
+		if i == ent:GetIndex() then
+			if v[2] >= g_curtime() then
+				b:Color(255,255,255,255)
+				draw.OutlinedRect(x, y, w, h)
+				b:AddTextBottom(v[1])
+			else
+				remove(made_noise, i)
+			end
+		end
+	end
+end)
