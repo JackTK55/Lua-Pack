718e8dc7be7b2618fd94d0053b2ee2ab9a3372da
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-01-14 13:38:42 -0500

    Update Draggable Menu.lua

--- luas/Draggable Menu.lua
+++ luas/Draggable Menu.lua
@@ -1,6 +1,17 @@
 local IsButtonDown, GetMousePos = input.IsButtonDown, input.GetMousePos
 local is_inside = function(a, b, x, y, w, h) return a >= x and a <= w and b >= y and b <= h end
 
+--[[
+	call: drag_menu(x, y, w, h)
+	ex: 
+	local x, y = drag_menu(
+		tX, 
+		tY, 
+		tX + 300, 
+		tY + 55
+	)
+]]
+
 local MENU = gui.Reference('MENU')
 local tX, tY = 300, 30 -- X, Y of menu spawn
 local offsetX, offsetY, _drag
