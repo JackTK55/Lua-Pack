46f45db9e279aad4fc5154908e0b7e5cfec09c14
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-02-14 19:26:29 -0500

    Update Translate.lua
    added different langs table for just the [en], etc. line 26 the or'' allows for symbols to be in text field.

--- luas/Translate.lua
+++ luas/Translate.lua
@@ -14,15 +14,20 @@
 
 local api_key = ''
 
+------------------------------------
+-----------	IGNORE BELOW -----------
+------------------------------------
+
 if api_key=='' then return error('api_key is empty.')end
 local url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 local translate_url = url..'?key='..api_key
 
-local ChatSay, Get, byte, tonumber, char, gsub, gmatch, Text = client.ChatSay, http.Get, string.byte, tonumber, string.char, string.gsub, string.gmatch, gui.Text
-local cth = function(c)return format("%%%02X",c:byte())end
+local ChatSay, Get, byte, tonumber, char, gsub, gmatch, Text, format, concat = client.ChatSay, http.Get, string.byte, tonumber, string.char, string.gsub, string.gmatch, gui.Text, string.format, table.concat
+local cth = function(c)return format("%%%02X",c:byte()or'')end
 local htc = function(x)return tonumber(x,16):char()end
 local encode = function(u)return u:gsub("\n","\r\n"):gsub("([^%w ])",cth):gsub(" ","+")end
 local decode = function(u)return u:gsub("+"," "):gsub("%%(%x%x)",htc)end
+local in_table=function(t,a)for i=1,#t do if t[i]==a then return true end end end
 
 local menu_open, MENU = true, gui.Reference('MENU')
 local w=gui.Window('lan_wm', 'Translation', 100, 100, 400, 405)
@@ -30,11 +35,21 @@ local g=gui.Groupbox(w,'Console Commands',15,15,185,125)Text(g,'-languages')Text
 local g2,g3=gui.Groupbox(w,'Languages',215,15,170,345),gui.Groupbox(w,'Translate',15,150,185,210)
 Text(g3,'From')local _FROM=gui.Editbox(g3,'translate_from','')Text(g3,'To')local _TO=gui.Editbox(g3,'translate_to','')Text(g3,'Text')local _TEXT=gui.Editbox(g3,'translate_text','')
 
-local langs = {}
-Get('https://pastebin.com/raw/UyHyYBPY', function(cnt)
+local langs, langs2 = {}, {}
+Get('https://pastebin.com/raw/udLBVaym', function(cnt)
 	for line in cnt:gmatch('([^\n]*)\n') do
 		langs[#langs + 1] = line
 		Text(g2, line)
+	end
+
+	for i=1,#langs do
+		local words = {}
+
+		for c in langs[i]:gmatch('([^->]*)$') do
+			words[#words + 1] = c:sub(2, -2)
+		end
+
+		langs2[#langs2 + 1] = words[1]
 	end
 end)
 
@@ -49,9 +64,7 @@ local get_translation = function(from, to, text)
 	end)
 end
 
-gui.Button(g3,'Say in Chat',function()
-	get_translation( _FROM:GetValue(), _TO:GetValue(), _TEXT:GetValue() )
-end)
+gui.Button(g3,'Say in Chat',function()get_translation(_FROM:GetValue(), _TO:GetValue(), _TEXT:GetValue())end)
 
 local function OnSendStringCmd(CMD)
 	local cmd = CMD:Get()
@@ -65,12 +78,25 @@ local function OnSendStringCmd(CMD)
 	end
 
 	if cmd:sub(1, 10) == '-translate' then
-		get_translation(cmd:sub(12, 13), cmd:sub(15, 16), cmd:sub(18))
+		local s, t, N = {}, {}, 1
+		for word in cmd:gmatch('([^ ]*)') do
+			if N > 3 then
+				t[#t + 1] = word
+			else
+				s[N] = word
+			end
+			N=N+1
+		end
+
+		local from = in_table(langs2, s[2]) and s[2] or 'en'
+		local to = in_table(langs2, s[3]) and s[3] or 'en'
+		local text = concat(t, ' ')
+
+		get_translation(from, to, text)
 		CMD:Set('')
 	end
 end
 
 callbacks.Register('SendStringCmd', OnSendStringCmd)
-
 gui.Button(gui.Reference('SETTINGS','Miscellaneous'),'Show/Hide Translation Window',function()menu_open=not menu_open end)
 callbacks.Register('Draw', function()w:SetActive(menu_open and MENU:IsActive())end)
