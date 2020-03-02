d4a904c8637306e54d22401ed88c9456878c5d83
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-03-02 06:56:09 -0500

    Update dragon_text-ui.lua
    v5

--- luas/dragon_text-ui.lua
+++ luas/dragon_text-ui.lua
@@ -10,80 +10,80 @@ local Color, TextShadow, SetFont, GetValue, type = draw.Color, draw.TextShadow, 
 local font = draw.CreateFont('Consolas', 15)
 
 local left_side = {
-	{'lbot_enable', 'LegitBot'},
-	{'lbot_trg_enable', 'TriggerBot'},
-	{'esp_enemy_box', 'Box ESP'},
-	{'esp_enemy_box_outline', 'Box Outline'},
-	{'esp_enemy_box_precise', 'Box Precision'},
-	{'esp_enemy_health', 'Health ESP'},
-	{'esp_enemy_armor', 'Armor ESP'},
-	{'esp_enemy_weapon', 'Weapon ESP'},
-	{'esp_enemy_ammo', 'Ammo ESP'},
-	{'esp_enemy_skeleton', 'Skeleton ESP'},
-	{'esp_enemy_hitbox', 'Hitbox ESP'},
-	{'esp_enemy_chams', 'Chams'},
-	{'esp_enemy_xqz', 'Chams XQZ'},
-	{'esp_enemy_glow', 'Glow'},
-	{'esp_enemy_headspot', 'Headspot'},
-	{'esp_enemy_barrel', 'Barrel'},
-	{'vis_historyticks', 'History Ticks'},
-	{'esp_weaponstyle', 'Weapon Style'},
-	{'vis_antiobs', 'Anti-OBS'},
-	{'vis_preservekillfeed', 'Preserve Killfeed'},
-	{'esp_outofview', 'Out of View Indicators'}
+	{'lbot.enable', 'LegitBot'},
+	{'lbot.trg.enable', 'TriggerBot'},
+	{'esp.enemy.box', 'Box ESP'},
+	{'esp.enemy.box.outline', 'Box Outline'},
+	{'esp.enemy.box.precise', 'Box Precision'},
+	{'esp.enemy.health', 'Health ESP'},
+	{'esp.enemy.armor', 'Armor ESP'},
+	{'esp.enemy.weapon', 'Weapon ESP'},
+	{'esp.enemy.ammo', 'Ammo ESP'},
+	{'esp.enemy.skeleton', 'Skeleton ESP'},
+	{'esp.enemy.hitbox', 'Hitbox ESP'},
+	{'esp.enemy.chams', 'Chams'},
+	{'esp.enemy.xqz', 'Chams XQZ'},
+	{'esp.enemy.glow', 'Glow'},
+	{'esp.enemy.headspot', 'Headspot'},
+	{'esp.enemy.barrel', 'Barrel'},
+	{'vis.historyticks', 'History Ticks'},
+	{'esp.weaponstyle', 'Weapon Style'},
+	{'vis.antiobs', 'Anti-OBS'},
+	{'vis.preservekillfeed', 'Preserve Killfeed'},
+	{'esp.outofview', 'Out of View Indicators'}
 }
 
 local top_right_side = {
-	{'msc_autojump', 'Auto Jump'},
-	{'msc_autostrafer_mode', 'Auto Strafe'},
-	{'msc_knifebot', 'KnifeBot'},
-	{'msc_logevents', 'Log Events'},
-	{'msc_fakelag_mode', 'Fakelag'},
-	{'msc_fakelatency_enable', 'Fakelatency'},
-	{'msc_hitmarker_enable', 'Hitmarker'},
-	{'msc_clantag', 'Clantag'}
+	{'msc.autojump', 'Auto Jump'},
+	{'msc.autostrafer.mode', 'Auto Strafe'},
+	{'msc.knifebot', 'KnifeBot'},
+	{'msc.logevents', 'Log Events'},
+	{'msc.fakelag.mode', 'Fakelag'},
+	{'msc.fakelatency.enable', 'Fakelatency'},
+	{'msc.hitmarker.enable', 'Hitmarker'},
+	{'msc.clantag', 'Clantag'}
 }
 
 local right_side = {
-	{'rbot_enable', 'RageBot'},
-	{'rbot_speedlimit', 'Speed Limit'},
-	{'rbot_silentaim', 'Silent Aim'},
-	{'rbot_resolver', 'Resolver'},
-	{'rbot_positionadjustment', 'Backtrack'},
-	{'rbot_delayshot', 'Delay Shot'},
-	{'rbot_doublefire', 'DoubleTap'},
-	{'rbot_antiaim_stand_pitch_real', 'Anti-Aim Pitch'},
-	{'rbot_antiaim_stand_real', 'Anti-Aim Yaw'},
-	{'rbot_antiaim_stand_desync', 'Desync'},
-	{'rbot_antiaim_at_targets', 'At Targets'},
-	{'rbot_antiaim_autodir', 'Auto Direction'}
+	{'rbot.enable', 'RageBot'},
+	{'rbot.speedlimit', 'Speed Limit'},
+	{'rbot.silentaim', 'Silent Aim'},
+	{'rbot.resolver', 'Resolver'},
+	{'rbot.positionadjustment', 'Backtrack'},
+	{'rbot.delayshot', 'Delay Shot'},
+	{'rbot.doublefire', 'DoubleTap'},
+	{'rbot.antiaim.stand.pitch.real', 'Anti-Aim Pitch'},
+	{'rbot.antiaim.stand.real', 'Anti-Aim Yaw'},
+	{'rbot.antiaim.stand.desync', 'Desync'},
+	{'rbot.antiaim.at.targets', 'At Targets'},
+	{'rbot.antiaim.autodir', 'Auto Direction'}
 }
 
 local var_to_mode = {
-	esp_enemy_box = {'Off', '2D', '3D', 'Edges', 'Machine', 'Pentagon', 'Hexagon'},
-	esp_enemy_health = {'Off', 'Bar', 'Number', 'Both'},
-	esp_enemy_weapon = {'Off', 'Show Active', 'Show All'},
-	esp_enemy_ammo = {'Off', 'Number', 'Bar'},
-	esp_enemy_hitbox = {'Off', 'White', 'Color'},
-	esp_enemy_chams = {'Off', 'Color', 'Material', 'Color Wireframe', 'Mat Wireframe', 'Invisible', 'Metallic', 'Flat'},
-	esp_enemy_glow = {'Off', 'Team Color', 'Health Color'},
-	vis_historyticks = {'Off', 'All Ticks', 'Last Tick', 'First Tick'},
-	esp_weaponstyle = {'Icons', 'Names'},
+	['esp.enemy.box'] = {'Off', '2D', '3D', 'Edges', 'Machine', 'Pentagon', 'Hexagon'},
+	['esp.enemy.health'] = {'Off', 'Bar', 'Number', 'Both'},
+	['esp.enemy.weapon'] = {'Off', 'Show Active', 'Show All'},
+	['esp.enemy.ammo'] = {'Off', 'Number', 'Bar'},
+	['esp.enemy.hitbox'] = {'Off', 'White', 'Color'},
+	['esp.enemy.chams'] = {'Off', 'Color', 'Material', 'Color Wireframe', 'Mat Wireframe', 'Invisible', 'Metallic', 'Flat'},
+	['esp.enemy.glow'] = {'Off', 'Team Color', 'Health Color'},
+	['vis.historyticks'] = {'Off', 'All Ticks', 'Last Tick', 'First Tick'},
+	['esp.weaponstyle'] = {'Icons', 'Names'},
 
-	msc_autojump = {'Off', 'Perfect', 'Legit'},
-	msc_autostrafer_mode = {'Silent', 'Normal', 'Sideways', 'W-Only', 'Mouse'},
-	msc_knifebot = {'Off', 'On', 'Backstab Only', 'Trigger', 'Quick'},
-	msc_fakelag_mode = {'Factor', 'Switch', 'Adaptive', 'Random', 'Peek', 'Rapid Peek'},
+	['msc.autojump'] = {'Off', 'Perfect', 'Legit'},
+	['msc.autostrafer.mode'] = {'Silent', 'Normal', 'Sideways', 'W-Only', 'Mouse'},
+	['msc.knifebot'] = {'Off', 'On', 'Backstab Only', 'Trigger', 'Quick'},
+	['msc.fakelag.mode'] = {'Factor', 'Switch', 'Adaptive', 'Random', 'Peek', 'Rapid Peek'},
 
-	rbot_speedlimit = {'Off', 'On', 'Auto'},
-	rbot_silentaim = {'Off', 'Client-Side', 'Server-Side'},
-	rbot_positionadjustment = {'Off', 'Low', 'Medium', 'High', 'Very High', 'Adaptive', 'Last Record'},
-	rbot_delayshot = {'Off', 'Accurate Unlag', 'Accurate History'},
-	rbot_antiaim_stand_pitch_real = {'Off', 'Emotion', 'Down', 'Up', 'Zero', 'Mixed', 'Custom', 'Shift'},
-	rbot_antiaim_stand_real = {'Off', 'Static', 'Spinbot', 'Jitter', 'Zero', 'Switch', 'Shift'},
-	rbot_antiaim_stand_desync = {'Off', 'Still', 'Balance', 'Stretch', 'Jitter'},
-	rbot_antiaim_at_targets = {'Off', 'Average', 'Closest'},
-	rbot_antiaim_autodir = {'Off', 'Default', 'Desync', 'Desync Jitter'}
+	['rbot.speedlimit'] = {'Off', 'On', 'Auto'},
+	['rbot.silentaim'] = {'Off', 'Client-Side', 'Server-Side'},
+	['rbot.positionadjustment'] = {'Off', 'Low', 'Medium', 'High', 'Very High', 'Adaptive', 'Last Record'},
+	['rbot.delayshot'] = {'Off', 'Accurate Unlag', 'Accurate History'},
+	['rbot.antiaim.stand.pitch.real'] = {'Off', 'Emotion', 'Down', 'Up', 'Zero', 'Mixed', 'Custom', 'Shift'},
+	['rbot.antiaim.stand.real'] = {'Off', 'Static', 'Spinbot', 'Jitter', 'Zero', 'Switch', 'Shift'},
+	['rbot.antiaim.stand.desync'] = {'Off', 'Still', 'Balance', 'Stretch', 'Jitter'},
+	['rbot.antiaim.at.targets'] = {'Off', 'Average', 'Closest'},
+	['rbot.antiaim.autodir'] = {'Off', 'Default', 'Desync', 'Desync Jitter'}
 }
 
 local function welcome()
@@ -104,31 +104,31 @@ local function main()
 		TextShadow(320, 81 + (i * 15), typ[2]..': '.. str)
 	end
 
-	local autostrafer = GetValue('msc_autostrafer_enable')
-	local fakelag = GetValue('msc_fakelag_enable')
+	local autostrafer = GetValue('msc.autostrafer.enable')
+	local fakelag = GetValue('msc.fakelag.enable')
 
 	for i=1, #top_right_side do
 		local typ = top_right_side[i]
 		local var = typ[1]
 		local str = type( GetValue(var) ) == 'boolean' and ( GetValue(var) and 'On' or 'Off' ) or var_to_mode[var][GetValue(var) + 1]
 
-		if var == 'msc_autostrafer_mode' then
+		if var == 'msc.autostrafer.mode' then
 			str = autostrafer and str or 'Off'
-		elseif var == 'msc_fakelag_mode' then
+		elseif var == 'msc.fakelag.mode' then
 			str = fakelag and str or 'Off'
 		end
 
 		TextShadow(550, 81 + (i * 15), typ[2]..': '.. str)
 	end
 
-	local rbot_aa_enabled = GetValue('rbot_antiaim_enable')
+	local rbot_aa_enabled = GetValue('rbot.antiaim.enable')
 
 	for i=1, #right_side do
 		local typ = right_side[i]
 		local var = typ[1]
 		local str = type( GetValue(var) ) == 'boolean' and ( GetValue(var) and 'On' or 'Off' ) or var_to_mode[var][GetValue(var) + 1]
 
-		if var == 'rbot_antiaim_stand_pitch_real' or var == 'rbot_antiaim_stand_real' or var == 'rbot_antiaim_stand_desync' or var == 'rbot_antiaim_at_targets' or var == 'rbot_antiaim_autodir' then
+		if var == 'rbot.antiaim.stand.pitch.real' or var == 'rbot.antiaim.stand.real' or var == 'rbot.antiaim.stand.desync' or var == 'rbot.antiaim.at.targets' or var == 'rbot.antiaim.autodir' then
 			str = rbot_aa_enabled and str or 'Off'
 		end
 
