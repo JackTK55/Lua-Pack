6cac0740804808de1962fc1c5113b2419ae012de
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-03-08 10:07:31 -0400

    Update [gs] team kills_damage.lua

--- luas/[gs] team kills_damage.lua
+++ luas/[gs] team kills_damage.lua
@@ -8,13 +8,13 @@ local drag_menu = function(x,y,w,h)if not is_menu_open()then return tX,tY end lo
 local mode = new_combobox('lua', 'a', 'Show Teammates Damage/Kills', 'Off', 'Without colors', 'Matchmaking colors')
 
 local colors = {
-	[-2]={200, 200, 200, 255}, -- bot
-	[-1]={200, 200, 200, 255}, -- gray
-	[0]={255, 255, 0, 255},	   -- yellow
-	[1]={110, 0, 255, 255},	   -- purple
-	[2]={0, 200, 0, 255},	   -- green
-	[3]={0, 75, 255, 255},	   -- blue
-	[4]={255, 145, 0, 255}	   -- orange
+	{200, 200, 200, 255}, -- bot
+	{200, 200, 200, 255}, -- gray
+	{255, 255, 0, 255},	  -- yellow
+	{110, 0, 255, 255},	  -- purple
+	{0, 200, 0, 255},	  -- green
+	{0, 75, 255, 255},	  -- blue
+	{255, 145, 0, 255}	  -- orange
 }
 
 local players, num_of_players = {}, 0
@@ -42,7 +42,7 @@ local function on_player_stuff(e)
 	end
 
 	if players[steamID3] == nil then
-		players[steamID3] = {0, 0, get_player_name(attacker), white, colors[get_prop(get_player_resource(), 'm_iCompTeammateColor', attacker)]}
+		players[steamID3] = {0, 0, get_player_name(attacker), white, colors[get_prop(get_player_resource(), 'm_iCompTeammateColor', attacker) + 3]}
 		num_of_players = num_of_players + 1
 	end
 
