512078c20306b6ccbf5393c1ea1a6c161e1d9f85
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-03-07 20:12:33 -0500

    Update [gs] team kills_damage.lua

--- luas/[gs] team kills_damage.lua
+++ luas/[gs] team kills_damage.lua
@@ -1,6 +1,7 @@
 local key_state, userid_to_entindex, read, write, get_local_player, get_player_name, get_prop, get_steam64, rectangle, text, get, is_menu_open, mouse_position, new_checkbox, pairs, set_callback, mp_td_dmgtokick = client.key_state, client.userid_to_entindex, database.read, database.write, entity.get_local_player, entity.get_player_name, entity.get_prop, entity.get_steam64, renderer.rectangle, renderer.text, ui.get, ui.is_menu_open, ui.mouse_position, ui.new_checkbox, pairs, ui.set_callback, cvar.mp_td_dmgtokick
 local _set, _unset = client.set_event_callback, client.unset_event_callback
 local is_inside = function(a, b, x, y, w, h) return a >= x and a <= w and b >= y and b <= h end
+local num_in_table = function(t)local n=0 for _ in pairs(t)do n=n+1 end return n end
 
 local enable = new_checkbox('lua', 'a', 'Show Teammates Damage/Kills')
 
@@ -65,7 +66,6 @@ local function on_player_stuff(e)
 	end
 end
 
-local gap = 0
 local function on_paint()
 	local x, y = drag_menu(tX, tY, 200, 20)
 
@@ -76,10 +76,9 @@ local function on_paint()
 	rectangle(x, y, 200, 20, 37, 37, 37, 250)
 	text(x + 100, y + 10, 255,255,255,255, 'c', 0, 'Player List')
 
-	rectangle(x, y, 200, gap, 33, 33, 33, 180)
+	rectangle(x, y, 200, num_in_table(players) * 10, 33, 33, 33, 180)
 
 	local y = y + 25
-
 	local dmg_to_kick = mp_td_dmgtokick:get_int()
 
 	local gap = 0
