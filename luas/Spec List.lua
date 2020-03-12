a23493df317805e8e92fc2f8a918b4146e8543ea
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-03-12 14:42:16 -0400

    Update Spec List.lua

--- luas/Spec List.lua
+++ luas/Spec List.lua
@@ -1,51 +1,71 @@
-local draw_GetScreenSize, entities_FindByClass, GetLocalPlayer, LocalPlayerIndex, GetPlayerResources, draw_SetFont, draw_GetTextSize, draw_Color, draw_TextShadow = draw.GetScreenSize, entities.FindByClass, entities.GetLocalPlayer, client.GetLocalPlayerIndex, entities.GetPlayerResources, draw.SetFont, draw.GetTextSize, draw.Color, draw.TextShadow
+local Register, GetLocalPlayerIndex, Color, CreateFont, GetScreenSize, GetTextSize, SetFont, TextShadow, FindByClass, GetLocalPlayer, GetPlayerResources, Checkbox, Reference = callbacks.Register, client.GetLocalPlayerIndex, draw.Color, draw.CreateFont, draw.GetScreenSize, draw.GetTextSize, draw.SetFont, draw.TextShadow, entities.FindByClass, entities.GetLocalPlayer, entities.GetPlayerResources, gui.Checkbox, gui.Reference
 
-local SpectatorList = gui.Checkbox(gui.Reference("MISC", "GENERAL", "Main"), "msc_speclist", "Spectators", false)
-local Vf12 = draw.CreateFont("Verdana", 12) 
+local enabled = Checkbox(Reference('Misc', 'General', 'Extra'), 'spec_list', 'Spectator List', false)
+enabled:SetDescription('Shows who is spectating you/whoever you are spectating')
 
-function SpecList()
-	if not SpectatorList:GetValue() or GetLocalPlayer() == nil then 
-		return 
-	end 
-	
-	local specX, specY = draw_GetScreenSize() 
-	local inbetween = 5 
-	local players = entities_FindByClass("CCSPlayer") 
-	
-	for i = 1, #players do 
-	
-		local player = players[i] 
-		local playername = player:GetName() 
-		local playerindex = player:GetIndex() 
-		local bot = GetPlayerResources():GetPropInt("m_iPing", playerindex) == 0
-		
-		if player:IsAlive() and not bot and player:GetPropEntity("m_hObserverTarget") ~= nil and playername ~= "GOTV" and playerindex ~= LocalPlayerIndex() then
-			local SpecTargetIndex = player:GetPropEntity("m_hObserverTarget"):GetIndex() 
-			
-			if GetLocalPlayer():IsAlive() then 
-				if SpecTargetIndex == LocalPlayerIndex() then 
-					draw_SetFont(Vf12) 
-					local tW, tH = draw_GetTextSize(playername) 
-					draw_Color(255,255,255,255) 
-					draw_TextShadow((specX - 5) - tW, inbetween, playername) 
-					inbetween = (inbetween + tH) + 5 
+local font = CreateFont('Verdana', 12, 200)
+
+local get_spectators = function()
+	local spectators, N = {}, 1
+	local lp = GetLocalPlayer()
+
+	if not lp then
+		return spectators
+	end
+
+	local lp_index = GetLocalPlayerIndex()
+	local lp_alive = lp:IsAlive()
+	local lp_obs_target = lp:GetPropEntity('m_hObserverTarget')
+	local players = FindByClass('CCSPlayer')
+	local player_resources = GetPlayerResources()
+
+	for i=1, #players do
+		local ent = players[i]
+		local ent_index = ent:GetIndex()
+		local obs_target = ent:GetPropEntity('m_hObserverTarget')
+
+		if obs_target and player_resources:GetPropInt('m_iPing', ent_index) > 0 and ent_index ~= lp_index then
+			local target_index = obs_target:GetIndex()
+
+			if lp_alive then
+				if not ent:IsAlive() and target_index == lp_index then
+					spectators[N] = ent
+					N = N + 1
 				end
-				
-			else 
-				if GetLocalPlayer():GetPropEntity("m_hObserverTarget") ~= nil then 
-					local imSpeccing = GetLocalPlayer():GetPropEntity("m_hObserverTarget"):GetIndex() 
-					
-					if SpecTargetIndex == imSpeccing then 
-						draw_SetFont(Vf12) 
-						local tW, tH = draw_GetTextSize(playername) 
-						draw_Color(255,255,255,255) 
-						draw_TextShadow((specX - 5) - tW, inbetween, playername) 
-						inbetween = (inbetween + tH) + 5 
+			else
+				if ent:IsAlive() and lp_obs_target then
+					if target_index == lp_obs_target:GetIndex() then
+						spectators[N] = ent
+						N = N + 1
 					end
 				end
-			end 
-		end 
-	end 
+			end
+		end
+	end
+
+	return spectators
 end
 
-callbacks.Register("Draw", SpecList)
+local function draw_spectators()
+	if not enabled:GetValue() then
+		return
+	end
+
+	local spectators = get_spectators()
+	local X = GetScreenSize()
+	local x = X - 5
+
+	for i=1, #spectators do
+		local entity = spectators[i]
+		local name = entity:GetName()
+
+		SetFont(font)
+		Color(255, 255, 255, 255)
+
+		local tW, tH = GetTextSize(name)
+
+		TextShadow(x - tW, i * (tH + 5), name)
+	end
+end
+
+Register('Draw', draw_spectators)
