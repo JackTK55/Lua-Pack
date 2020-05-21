7497b66f4e215ee655bc36c47370495d33d0a55c
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-05-21 14:14:12 -0400

    Update Fast Plant.lua

--- luas/Fast Plant.lua
+++ luas/Fast Plant.lua
@@ -1,8 +1,10 @@
 local GetLocalPlayer, IsButtonDown = entities.GetLocalPlayer, input.IsButtonDown
-local r = gui.Reference('MISC', 'AUTOMATION', 'Other')
+local r = gui.Reference('Misc', 'Movement', 'Other')
 local enabled = gui.Checkbox(r, 'fast_plant', 'Fast Plant', false)
-local hotkey = gui.Keybox(r, 'fast_plant_key', 'Fast Plant Key', 0) -- Don't make it "e" or "mouse 1"
+local hotkey = gui.Keybox(r, 'fast_plant_key', 'Fast Plant Key', 0)
+hotkey:SetDescription("Don't use \"E\" or \"Mouse1\"")
 
+local in_use = bit.lshift(1, 5)
 callbacks.Register('CreateMove', function(cmd)
 	if not enabled:GetValue() then
 		return
@@ -21,6 +23,6 @@ callbacks.Register('CreateMove', function(cmd)
 	local in_air = flag == 256 or flag == 262
 
 	if in_zone and not in_air then 
-		cmd:SetButtons( (1 << 5) )
+		cmd:SetButtons( in_use )
 	end 
 end)
