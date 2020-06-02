392cb1744d769e89665846dd2db36ab4dd88824e
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-06-02 17:09:52 -0400

    changed link to actual good link

--- luas/Team Based Glove&Knife.lua
+++ luas/Team Based Glove&Knife.lua
@@ -1,5 +1,5 @@
 --[[
-	Team Based Skins has many more options -> https://raw.githubusercontent.com/Zack2kl/Lua-Pack/master/luas/Team%20Based%20Skins.lua
+	Team Based Skins has many more options -> https://github.com/Zack2kl/Team-Based-Skins/blob/master/team_based_skins.lua
 ]]
 
 local GetLocalPlayer, client_exec, GetValue, SetValue, LocalPlayerIndex, PlayerIndexByUserID, MENU = entities.GetLocalPlayer, client.Command, gui.GetValue, gui.SetValue, client.GetLocalPlayerIndex, client.GetPlayerIndexByUserID, gui.Reference("MENU")
