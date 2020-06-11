288a2d65a60b12e8eb58540c3d9d075dec274698
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-06-11 09:24:36 -0400

    Update Aspect Ratio Changer.lua
    not sure about the math, got it from old lua

--- luas/Aspect Ratio Changer.lua
+++ luas/Aspect Ratio Changer.lua
@@ -1,22 +1,25 @@
-local SS,S,s,r,ref=draw.GetScreenSize,client.SetConVar,false,'r_aspectratio',gui.Reference('MISC','GENERAL','Main')
+local ref = gui.Reference('Misc', 'General')
+local active = gui.Checkbox(ref, 'aspect_ratio_active', 'Aspect Ratio Changer', false)
+local ratio = gui.Slider(ref, 'aspect_ratio_value', 'Aspect Ratio', 100, 1, 199)
+local GetScreenSize, SetConVar = draw.GetScreenSize, client.SetConVar
 
-local enabled = gui.Checkbox(ref,'lua_aspect_ratio_changer_enable','Aspect Ratio Changer',false)
-local ratio = gui.Slider(ref,'lua_aspect_ratio_changer_value','Force aspect ratio',100, 1, 199)
-
-local function on_aspect_ratio_changed()
-	if enabled:GetValue() then
-		local w, h = SS()
-		local a = 2 - (ratio:GetValue() * 0.01)
-		local ar = (w * a) / h
-		local v = a == 1 and 0 or ar
-		S(r,v,true)
-		s = true
-	else
-		if s then
-			S(r,0,true)
-			s = false
+local last
+local function main()
+	if not active:GetValue() then
+		if last then
+			SetConVar('r_aspectratio', 0, true)
+			last = nil
 		end
+		return
+	end
+
+	local new = 2 - (ratio:GetValue() * 0.01)
+	if last ~= new then
+		local w, h = GetScreenSize()
+		local v = new == 1 and 0 or ( (w * new) / h )
+		SetConVar('r_aspectratio', v, true)
+		last = new
 	end
 end
 
-callbacks.Register('Draw', on_aspect_ratio_changed)
+callbacks.Register('Draw', main)
