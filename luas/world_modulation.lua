b2d15b6f0fe0e5bb1b422537509d850481c0746e
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-06-12 20:03:58 -0400

    Update world_modulation.lua
    updated to V5

--- luas/world_modulation.lua
+++ luas/world_modulation.lua
@@ -2,14 +2,16 @@
 -- Cheeseot edited original
 -- zack edited that edit
 
-local SetConVar, FindByClass, GetLocalPlayer, pairs = client.SetConVar, entities.FindByClass, entities.GetLocalPlayer, pairs
+local SetConVar, FindByClass, GetLocalPlayer = client.SetConVar, entities.FindByClass, entities.GetLocalPlayer
 local m, m2 = 1 / 255, 1 / 100
 
-local r = gui.Reference("VISUALS", "MISC", "World")
-local colors = gui.ColorEntry('clr_world_modulation', 'World Modulation', 255, 0, 0, 255)
-local sli_exposure = gui.Slider(r, "nex_bloom_exposure", "World Exposure", 100, 1, 100)
-local sli_modelAmbient = gui.Slider(r, "nex_bloom_model_ambient", "Model Ambient", 1, 1, 100)
-local sli_bloom = gui.Slider(r, "nex_bloom_scale", "Bloom Scale", 20, 1, 100)
+local tab = gui.Tab(gui.Reference('Visuals'), 'world_modulation', 'World Modulation')
+local group = gui.Groupbox(tab, 'Modulation', 16, 16)
+
+local colors = gui.ColorPicker(group, 'world_modulation', 'World Modulation', 255, 0, 0, 255)
+local sli_exposure = gui.Slider(group, 'sli_exposure', 'World Exposure', 100, 1, 100)
+local sli_modelAmbient = gui.Slider(group, 'sli_modelAmbient', 'Model Ambient', 1, 1, 100)
+local sli_bloom = gui.Slider(group, 'sli_bloom', 'Bloom Scale', 20, 1, 100)
 
 local var = {
 	R = 0,
@@ -21,7 +23,7 @@ local var = {
 	Bloom = 0
 }
 
-callbacks.Register("Draw", "Modulation", function()
+callbacks.Register('Draw', function()
 	if not GetLocalPlayer() then
 		return
 	end
@@ -30,21 +32,21 @@ callbacks.Register("Draw", "Modulation", function()
 	local r, g, b, a = r * m, g * m, b * m, a * m
 
 	if var.R ~= r then
-		SetConVar("mat_ambient_light_r", r, true)
+		SetConVar('mat_ambient_light_r', r, true)
 		var.R = r
 	end
 
 	if var.G ~= g then
-		SetConVar("mat_ambient_light_g", g, true)
+		SetConVar('mat_ambient_light_g', g, true)
 		var.G = g
 	end
 
 	if var.B ~= b then
-		SetConVar("mat_ambient_light_b", b, true)
+		SetConVar('mat_ambient_light_b', b, true)
 		var.B = b
 	end
 
-	local con = FindByClass("CEnvTonemapController")[1]
+	local con = FindByClass('CEnvTonemapController')[1]
 
 	if not con then
 		return
@@ -55,25 +57,25 @@ callbacks.Register("Draw", "Modulation", function()
 	local sli_mA = sli_modelAmbient:GetValue()
 
 	if var.Props == 0 then
-		con:SetProp("m_bUseCustomAutoExposureMin", 1)
-		con:SetProp("m_bUseCustomAutoExposureMax", 1)
-		con:SetProp("m_bUseCustomBloomScale", 1)
+		con:SetProp('m_bUseCustomAutoExposureMin', 1)
+		con:SetProp('m_bUseCustomAutoExposureMax', 1)
+		con:SetProp('m_bUseCustomBloomScale', 1)
 		var.Props = 1
 	end
 
 	if var.Exposure ~= sli_e and var.Props == 1 then
-		con:SetProp("m_flCustomAutoExposureMin", sli_e)
-		con:SetProp("m_flCustomAutoExposureMax", sli_e)
+		con:SetProp('m_flCustomAutoExposureMin', sli_e)
+		con:SetProp('m_flCustomAutoExposureMax', sli_e)
 		var.Exposure = sli_e
 	end
 
 	if var.Bloom ~= sli_b and var.Props == 1 then
-		con:SetProp("m_flCustomBloomScale", sli_b)
+		con:SetProp('m_flCustomBloomScale', sli_b)
 		var.Bloom = sli_b
 	end
 
 	if var.Ambient ~= sli_mA then
-		SetConVar("r_modelAmbientMin", sli_mA, true)
+		SetConVar('r_modelAmbientMin', sli_mA, true)
 		var.Ambient = sli_mA
 	end
 end)
