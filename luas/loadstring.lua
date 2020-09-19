d58de19a2ae9ab0a3f9f3390046e5d9941cfff19
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-09-19 08:45:24 -0400

    Update loadstring.lua

--- luas/loadstring.lua
+++ luas/loadstring.lua
@@ -18,20 +18,23 @@ if not loadstring_old then
     end
 end
 
-
 -- Example 1; obfuscated
 http.Get( 'https://pastebin.com/raw/SZLF0Db1', loadstring )
 
 
 -- Example 2
 loadstring([[
-    return(function() 
-        callbacks.Register( 'Draw', function()
-            draw.Color(33, 33, 33, 190)
-            draw.FilledRect(0, 30, 267, 54)
-        
-            draw.Color( 255, 0, 100, 255 )
-            draw.Text( 6, 36, 'This is drawn using Example 2' )
-        end)
-    end)()
+    callbacks.Register( 'Draw', function()
+        draw.Color(33, 33, 33, 190)
+        draw.FilledRect(0, 30, 267, 54)
+    
+        draw.Color( 255, 0, 100, 255 )
+        draw.Text( 6, 36, 'This is drawn using Example 2' )
+    end)
+
+    local script_name = GetScriptName()
+    local ref = gui.Reference( 'Settings', 'Advanced' )
+    local button = gui.Button( ref, 'Unload: '.. script_name, function( )
+        UnloadScript( script_name )
+    end)
 ]])
