b4bb32bb12d08aa426644097f993be39927821ad
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-12-28 21:14:42 -0500

    Create [gs] Text to Speech.lua
    Both work in progress but works when I use it

---
+++ luas/[gs] Text to Speech.lua
@@ -0,0 +1,226 @@
+local http = require 'gamesense/http'
+
+-- http://www.voicerss.org/api/
+local API_KEY = ''
+
+
+----------- This lua is a work in progress -----------
+
+----------- EDIT NOTHING BELOW -----------
+----------- EDIT NOTHING BELOW -----------
+----------- EDIT NOTHING BELOW -----------
+----------- EDIT NOTHING BELOW -----------
+
+
+local data = (function()
+	local f, data = pcall( readfile, 'csgo/sound/tts/data.json' )
+
+	if not f then
+		writefile( 'csgo/sound/tts/data.json', '{}' )
+	end
+
+	return f and json.parse(data) or {}
+end)()
+
+local get_desc = function( tbl )
+	local info = {}
+
+	for index, item in next, tbl do
+		info[index] = item[1]
+	end
+
+	return info
+end
+
+local random_name = function()
+	local s = ''
+
+	for i=1, 10 do
+		s = s .. client.random_int(0, 9)
+	end
+
+	return s
+end
+
+local item_in_tbl = function(v, t)
+	for i=1, #t do
+		if v == t[i] then
+			return true
+		end
+	end
+	return false
+end
+
+local opts = {
+	'Console Input (tts "message")',
+	'Game Chat Input (/tts "message")',
+	'Show Saved Text-To-Speech',
+	'Auto TTS Sent Team Chat',
+	'Output Info in Console'
+}
+
+local options = ui.new_multiselect( 'misc', 'miscellaneous', 'Text-To-Speech Options:', opts )
+local descriptions = get_desc( data )
+local previous_sounds = ui.new_listbox( 'misc', 'miscellaneous', 'Previous Text-To-Speech', descriptions )
+
+local play_sound_t = vtable_bind( 'vguimatsurface.dll', 'VGUI_Surface031', 82, 'void(__thiscall*)(void*, const char*)' )
+local console_output = false
+
+local function printf(endl, ...)
+	return console_output and print( table.concat({...}, '') .. (endl == 1 and '\n' or '') )
+end
+
+local function play_sound( file, dur )
+	cvar.voice_inputfromfile:set_int( 1 )
+	cvar['+voicerecord']:invoke_callback()
+
+	play_sound_t( file )
+	printf( 1, 'Playing Sound: ', file )
+
+	client.delay_call( tonumber(dur), function()
+		cvar['-voicerecord']:invoke_callback()
+		cvar.voice_inputfromfile:set_int( 0 )
+	end)
+end
+
+local send_button = ui.new_button( 'misc', 'miscellaneous', 'Send Selected TTS', function()
+	local selected = (ui.get(previous_sounds) or -1) + 1
+	local info = data[selected]
+
+	if not info[1] then
+		return
+	end
+
+	printf( 0, 'Text-To-Speech: ', info[1] )
+	printf( 0, ('Sound has a duration of: %0.3f seconds!'):format(info[3]) )
+
+	play_sound( 'tts/'.. info[2] ..'.mp3', info[3] )
+end)
+
+local remove_button = ui.new_button( 'misc', 'miscellaneous', 'Remove Selected TTS', function()
+	local selected = (ui.get(previous_sounds) or -1) + 1
+	local info = data[selected]
+
+	if not info[1] then
+		return
+	end
+
+	printf( 0, 'Removed: ', info[1] )
+
+	table.remove( descriptions, selected )
+	table.remove( data, selected )
+
+	ui.update( previous_sounds, descriptions )
+	ui.set( previous_sounds, selected - 2 )
+end)
+
+local function send_request( text )
+	if not text then
+		return
+	end
+
+	local url = 'https://api.voicerss.org/'
+	local key = API_KEY
+	local lang = 'en-us'
+	local start = globals.curtime()
+	local file_name = random_name()
+
+	http.get( url, {params = {key=key, lang=lang, c='mp3', f='alaw_22khz_mono', v='Mike', src=text}}, function(success, response)
+		if not success or response.status ~= 200 then
+			for k, v in pairs( response ) do
+				printf( 0, 'ERROR: ', k, ' ', v )
+			end
+
+			return printf( 1, 'ERROR: Failed to get TTS' )
+		end
+
+		local file_src = response.body
+
+		if #file_src < 75 then
+			return error( file_src, 2 )
+		end
+
+		printf( 0, ('Downloaded sound in: %0.3f seconds!'):format(globals.curtime() - start) )
+
+		local dur = (#file_src * 4) / 22000
+		local dur_f = ('%0.3f'):format(dur)
+		printf( nil, ('Sound has a duration of: %s seconds!'):format(dur_f) )
+
+		writefile( 'csgo/sound/tts/'.. file_name ..'.mp3', file_src )
+
+		descriptions[#descriptions + 1] = text
+		data[#data + 1] = {text, file_name, dur_f}
+
+		ui.update( previous_sounds, descriptions )
+
+		play_sound( 'tts/'.. file_name ..'.mp3', dur )
+	end)
+end
+
+local function on_console_input(input)
+	repeat
+		local tts_text = input:match( 'tts (.*)' )
+
+		if not tts_text then
+			break
+		end
+
+		printf( 0, 'Text-To-Speech: ', tts_text )
+		send_request( tts_text )
+
+		return true
+	until 1
+end
+
+local function on_string_cmd(input)
+	repeat
+		local cmd = input.text
+
+		local all_text = cmd:match( 'say "/tts(.*)"' )
+		local team_text = cmd:match( 'say_team "/tts(.*)"' )
+		local team_text_2 = auto_tts and cmd:match( 'say_team "(.*)"' )
+
+		if not all_text and not team_text and not team_text_2 then
+			break
+		end
+
+		local str = all_text or team_text or (auto_tts and team_text_2) and team_text_2
+		printf( 0, 'Text-To-Speech: ', str )
+		send_request( str )
+
+		return true
+	until 1
+end
+
+local callbacks = {
+	[true] = client.set_event_callback,
+	[false] = client.unset_event_callback
+}
+
+local function on_selection_change()
+	local tbl = ui.get(options)
+
+	local v1 = item_in_tbl( opts[1], tbl )
+	local v2 = item_in_tbl( opts[2], tbl )
+	auto_tts = item_in_tbl( opts[4], tbl )
+	console_output = item_in_tbl( opts[5], tbl )
+
+	callbacks[v1]( 'console_input', on_console_input )
+	callbacks[v2]( 'string_cmd', on_string_cmd )
+
+	local list = item_in_tbl( opts[3], tbl )
+	ui.set_visible( previous_sounds, list )
+	ui.set_visible( send_button, list )
+	ui.set_visible( remove_button, list )
+end
+
+on_selection_change()
+ui.set_callback( options, on_selection_change )
+
+client.set_event_callback( 'shutdown', function()
+	for i, info in next, data do
+		printf( 0, i, ' = {', table.concat(info, ', '), '}' )
+	end
+
+	writefile( 'csgo/sound/tts/data.json', json.stringify(data) )
+end)
