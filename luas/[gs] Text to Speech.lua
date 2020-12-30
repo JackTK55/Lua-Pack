e2a4692082048b2253ae538b643cce9142988fb8
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2020-12-29 21:28:18 -0500

    Update [gs] Text to Speech.lua

--- luas/[gs] Text to Speech.lua
+++ luas/[gs] Text to Speech.lua
@@ -1,5 +1,3 @@
-local http = require 'gamesense/http'
-
 -- http://www.voicerss.org/api/
 local API_KEY = ''
 
@@ -11,15 +9,16 @@ local API_KEY = ''
 ----------- EDIT NOTHING BELOW -----------
 ----------- EDIT NOTHING BELOW -----------
 
+local http = require 'gamesense/http'
 
 local data = (function()
-	local f, data = pcall( readfile, 'csgo/sound/tts/data.json' )
+	local success, data = pcall( readfile, 'csgo/sound/tts/data.json' )
 
-	if not f then
+	if not success then
 		writefile( 'csgo/sound/tts/data.json', '{}' )
 	end
 
-	return f and json.parse(data) or {}
+	return data and json.parse(data) or {}
 end)()
 
 local get_desc = function( tbl )
@@ -52,10 +51,10 @@ local item_in_tbl = function(v, t)
 end
 
 local opts = {
-	'Console Input (tts "message")',
-	'Game Chat Input (/tts "message")',
+	'Console Input (tts "msg")',
+	'Game Chat Input (/tts "msg")',
+	'Auto TTS Sent Team Chat ("msg")',
 	'Show Saved Text-To-Speech',
-	'Auto TTS Sent Team Chat',
 	'Output Info in Console'
 }
 
@@ -70,48 +69,54 @@ local function printf(endl, ...)
 	return console_output and print( table.concat({...}, '') .. (endl == 1 and '\n' or '') )
 end
 
+local loop_back = 0
+cvar.snd_restart:invoke_callback()
 local function play_sound( file, dur )
+	loop_back = cvar.voice_loopback:get_int()
+
 	cvar.voice_inputfromfile:set_int( 1 )
+	cvar.voice_loopback:set_int( 1 )
 	cvar['+voicerecord']:invoke_callback()
 
-	play_sound_t( file )
 	printf( 1, 'Playing Sound: ', file )
+	play_sound_t( file )
 
 	client.delay_call( tonumber(dur), function()
 		cvar['-voicerecord']:invoke_callback()
 		cvar.voice_inputfromfile:set_int( 0 )
+		cvar.voice_loopback:set_int( loop_back )
 	end)
 end
 
 local send_button = ui.new_button( 'misc', 'miscellaneous', 'Send Selected TTS', function()
-	local selected = (ui.get(previous_sounds) or -1) + 1
-	local info = data[selected]
+	local sel = (ui.get(previous_sounds) or -1) + 1
+	local info = data[sel]
 
-	if not info[1] then
+	if not info or not info[1] then
 		return
 	end
 
 	printf( 0, 'Text-To-Speech: ', info[1] )
 	printf( 0, ('Sound has a duration of: %0.3f seconds!'):format(info[3]) )
-
 	play_sound( 'tts/'.. info[2] ..'.mp3', info[3] )
 end)
 
 local remove_button = ui.new_button( 'misc', 'miscellaneous', 'Remove Selected TTS', function()
-	local selected = (ui.get(previous_sounds) or -1) + 1
-	local info = data[selected]
+	local sel = (ui.get(previous_sounds) or -1) + 1
+	local info = data[sel]
 
-	if not info[1] then
+	if not info or not info[1] then
 		return
 	end
 
+	table.remove( descriptions, sel )
+	table.remove( data, sel )
+
+	writefile( 'csgo/sound/tts/'.. info[2] ..'.mp3', ' ' )
 	printf( 0, 'Removed: ', info[1] )
 
-	table.remove( descriptions, selected )
-	table.remove( data, selected )
-
 	ui.update( previous_sounds, descriptions )
-	ui.set( previous_sounds, selected - 2 )
+	ui.set( previous_sounds, sel - 2 )
 end)
 
 local function send_request( text )
@@ -122,35 +127,38 @@ local function send_request( text )
 	local url = 'https://api.voicerss.org/'
 	local key = API_KEY
 	local lang = 'en-us'
+	local voice = 'Mike'
 	local start = globals.curtime()
 	local file_name = random_name()
 
-	http.get( url, {params = {key=key, lang=lang, c='mp3', f='alaw_22khz_mono', v='Mike', src=text}}, function(success, response)
+	http.get( url, {params = {key=key, lang=lang, c='mp3', f='22khz_8bit_mono', v=voice, src=text}}, function(success, response)
 		if not success or response.status ~= 200 then
+			printf( 1, 'ERROR: Failed to get TTS' )
+
 			for k, v in pairs( response ) do
 				printf( 0, 'ERROR: ', k, ' ', v )
 			end
 
-			return printf( 1, 'ERROR: Failed to get TTS' )
+			return
 		end
 
 		local file_src = response.body
+		local file_size = #file_src
 
-		if #file_src < 75 then
+		if file_size < 75 then
 			return error( file_src, 2 )
 		end
 
 		printf( 0, ('Downloaded sound in: %0.3f seconds!'):format(globals.curtime() - start) )
 
-		local dur = (#file_src * 4) / 22000
+		local dur = (file_size * 8) / 48000
 		local dur_f = ('%0.3f'):format(dur)
-		printf( nil, ('Sound has a duration of: %s seconds!'):format(dur_f) )
 
+		printf( nil, ('Sound has a duration of: %s seconds!'):format(dur_f) )
 		writefile( 'csgo/sound/tts/'.. file_name ..'.mp3', file_src )
 
 		descriptions[#descriptions + 1] = text
-		data[#data + 1] = {text, file_name, dur_f}
-
+		data[#data + 1] = { text, file_name, dur_f }
 		ui.update( previous_sounds, descriptions )
 
 		play_sound( 'tts/'.. file_name ..'.mp3', dur )
@@ -202,13 +210,13 @@ local function on_selection_change()
 
 	local v1 = item_in_tbl( opts[1], tbl )
 	local v2 = item_in_tbl( opts[2], tbl )
-	auto_tts = item_in_tbl( opts[4], tbl )
+	auto_tts = item_in_tbl( opts[3], tbl )
 	console_output = item_in_tbl( opts[5], tbl )
 
 	callbacks[v1]( 'console_input', on_console_input )
 	callbacks[v2]( 'string_cmd', on_string_cmd )
 
-	local list = item_in_tbl( opts[3], tbl )
+	local list = item_in_tbl( opts[4], tbl )
 	ui.set_visible( previous_sounds, list )
 	ui.set_visible( send_button, list )
 	ui.set_visible( remove_button, list )
